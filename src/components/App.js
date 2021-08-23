c0d0fe2f8242f4026bbd390c1084a5441b5ca7af
Christopher <cristoferperezmejia80@gmail.com>
2021-08-23 18:07:15 -0400

    styles to grid containers added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,17 @@
+import SunnySide from "./SunnySide/SunnySide";
+
+import emilyImg from "./SunnySide/assets/images/image-emily.jpg";
+import thomasImg from "./SunnySide/assets/images/image-thomas.jpg";
+import jennieImg from "./SunnySide/assets/images/image-jennie.jpg";
+
+const testimonials = [
+  { userImg: emilyImg, userTestimonial: "We put trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit. ", userName: "Emely R.", userOcupation: "Markting Director" },
+  { userImg: thomasImg, userTestimonial: "SunnySide's enthusiasm coupled with their keen interest in our brand's success made it a setisfysing and enjoyable expirience.", userName: "Thomas S.", userOcupation: "Chief Operating Officer" },
+  { userImg: jennieImg, userTestimonial: "Incredibly end Result! Our sales increased over 400% when worked with Sunnyside. Highly recommended!", userName: "Jennie F.", userOcupation: "Business Owner" },
+];
+
 function App() {
-  return <div></div>;
+  return <SunnySide testimonials={testimonials} />;
 }
 
 export default App;
---
+++ src/components/SunnySide/SunnySide.js
@@ -0,0 +1,107 @@
+import React from "./SunnySide";
+
+import "./assets/css/normalize.css";
+import "./assets/css/SunnySide.css";
+
+import logo from "./assets/images/logo.svg";
+import hamburgerIcon from "./assets/images/icon-hamburger.svg";
+
+const SunnySide = (props) => {
+  console.log(props.testimonials);
+  return (
+    <>
+      <header className="headerContainer">
+        <div className="logoContainer">
+          <img src={logo} alt="logo" />
+          <img className="hamburgerIcon" src={hamburgerIcon} alt="a hamburger icon" />
+        </div>
+        <div className="modalContainer">
+          <ul className="menuContainer">
+            <li className="muneItem">About</li>
+            <li className="muneItem">Services</li>
+            <li className="muneItem">Projects</li>
+            <li className="muneItem">CONTACT</li>
+          </ul>
+        </div>
+        <div className="titleContainer">
+          <h1 className="title">
+            {" "}
+            <span className="we-are">WE ARE</span>CREATIVES
+          </h1>
+        </div>
+      </header>
+      <main>
+        {" "}
+        <Main />
+        <Testimonials testimonials={props.testimonials} />
+      </main>
+    </>
+  );
+};
+
+const Main = () => {
+  return (
+    <>
+      <div className="imagesGridContainer">
+        {/* Transfom Container */}
+        <div className="transformContainer">
+          <div className="transformImgContainer"></div>
+          <div className="transformTextContainer">
+            <h2 className="transformTitle">Transform your brand</h2>
+            <p className="transformParagraph">We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
+            <a className="transformLink" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" rel="noreferrer" target="_blank">
+              {" "}
+              LEARN MORE
+            </a>
+          </div>
+        </div>
+
+        {/* Stand out Container */}
+        <div className="standOutContainer">
+          <div className="standOutImgContainer"></div>
+          <div className="standOutTextContainer">
+            <h2 className="standOutTitle">Stand Out to the audience</h2>
+            <p className="standOutParagraph">Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extand your brand in digital places.</p>
+          </div>
+          <a className="standOutLink" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" rel="noreferrer" target="_blank">
+            {" "}
+            LEARN MORE
+          </a>
+        </div>
+
+        {/* Graphic Container */}
+        <div className="graphicContainer">
+          <h2 className="graphicTitle">Graphic Design</h2>
+          <p className="graphicParagraph">Great design makes you memorable. We deliver atwork that underscores your brand message and captures potential clients' attention.</p>
+        </div>
+
+        {/*Photography Container  */}
+        <div className="photographyContainer">
+          <h2 className="photographyTitle">Photography</h2>
+          <p className="photographyParagraph">Increase your credibility by getting the most stunning hegh-quality photos that improve your business image.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Testimonials = (props) => {
+  console.log(props);
+  return (
+    <div className="testimonailContainers">
+      {/* <h3>CLIENT TESTIMONAILS</h3> */}
+      {props.testimonials.map((testimonial) => {
+        return (
+          <div>
+            <img src={testimonial.userImg} alt="user img" />
+            <p>{testimonial.userTestimonial}</p>
+            <span>{testimonial.userName}</span>
+            <span>{testimonial.userOcupation}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SunnySide;
